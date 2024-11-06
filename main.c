754b645333f3f4353175503b161e4d3e8109ac72
Rong Tao <rongtao@cestc.cn>
2024-11-06 12:23:27 +0800

    Add main.c
    Signed-off-by: Rong Tao <rongtao@cestc.cn>

---
+++ main.c
@@ -0,0 +1,13 @@
+/**
+ * File: main.c
+ *
+ * This source code just for test.
+ *
+ * Copyright (C) CESTC, Co. 2024. All rights reserved.
+ */
+#include <stdio.h>
+
+int main(void)
+{
+	return 0;
+}
